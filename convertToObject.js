c82b6b836d01732b5fe6136dab5a3f6019b5433b
eternal034 <97731724+eternal034@users.noreply.github.com>
2022-01-14 12:34:05 +0200

    Delete convertToObject.js

--- convertToObject.js
+++
@@ -1,9 +0,0 @@
-function converting(json){
-
-    let person = JSON.parse(json);
-    for(let key of Object.keys(person)){
-        console.log(`${key}: ${person[key]}`);
-    }
-}
-
-converting('{"name": "George", "age": 40, "town": "Sofia"}');
